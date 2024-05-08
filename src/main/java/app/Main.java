5e8532b52bd805df27dbb60de799b4a49e1ba70a
Tobitastik <144216903+Tobitastik@users.noreply.github.com>
2024-05-08 16:56:01 +0200

    Added the fragment to the html and connected the pages. Added the "Tak for din bestilling"-page. Changed a few of the images ending to lowercase to be consistent with the other images

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -5,20 +5,20 @@ import app.config.ThymeleafConfig;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
 
-
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // Initializing Javalin and Jetty webserver
-
         Javalin app = Javalin.create(config -> {
             config.staticFiles.add("/public");
-            config.jetty.modifyServletContextHandler(handler ->  handler.setSessionHandler(SessionConfig.sessionConfig()));
+            config.jetty.modifyServletContextHandler(handler -> handler.setSessionHandler(SessionConfig.sessionConfig()));
             config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
         }).start(7070);
 
         // Routing
+        app.get("/", ctx -> ctx.render("carport-index.html"));
+        app.get("/carport-form", ctx -> ctx.render("carport-form.html"));
+        app.get("/customer-info-page", ctx -> ctx.render("customer-info-page.html"));
 
-        app.get("/", ctx ->  ctx.render("index.html"));
+        app.post("/carport-offer-sent", ctx -> ctx.render("carport-offer-sent.html"));
     }
 }
