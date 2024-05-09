08c8c883ae4dea1d50a49597ebd6303d72ec7a69
Tobitastik <144216903+Tobitastik@users.noreply.github.com>
2024-05-09 15:30:32 +0200

    CustomerRequestController started

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,10 +2,17 @@ package app;
 
 import app.config.SessionConfig;
 import app.config.ThymeleafConfig;
+import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
 
 public class Main {
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "HigAbt60ig";
+    private static final String URL = "jdbc:postgresql://161.35.195.156/%s?currentSchema=public";
+    private static final String DB = "carport";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
     public static void main(String[] args) {
         // Initializing Javalin and Jetty webserver
         Javalin app = Javalin.create(config -> {
--- src/main/java/app/controllers/CustomerRequestController.java
+++ src/main/java/app/controllers/CustomerRequestController.java
@@ -1,11 +1,36 @@
 package app.controllers;
 
+import app.entities.CustomerRequest;
 import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
-import app.persistence.CustomerMapper;
+
+import app.persistence.CustomerRequestMapper;
+import io.javalin.Javalin;
 import io.javalin.http.Context;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public class CustomerRequestController {
 
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
+
+    }
+    private static void addCustomerRequest(Context ctx, ConnectionPool connectionPool) {
+        int length = Integer.parseInt(ctx.formParam("length"));
+        int width = Integer.parseInt(ctx.formParam("width"));
+        int height = Integer.parseInt(ctx.formParam("height"));
+        LocalDate localDate = LocalDate.now();
+
+        try {
+            CustomerRequestMapper.makeCustomerRequest(length, width, height, localDate, connectionPool);
+            List<CustomerRequest> customerRequestList = CustomerRequestMapper.getAllCustomerRequest(connectionPool);
+
+            ctx.render("carport-offer-sent.html");
+        } catch (DatabaseException e){
+            ctx.attribute("message", "Please try again");
+            ctx.render("carport-form.html");
+        }
+    }
 
 }
