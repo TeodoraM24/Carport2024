c92a37c4745ce7c58c1e818d76384348f588fdf4
Ømer <cph-oo203@cphbusiness.dk>
2024-05-13 16:51:01 +0200

    added CarportSvgTopDownView Class, Svg Class, Svg-page.html, SvgController Class and routing in Main

---
+++ src/main/java/app/controllers/SvgController.java
@@ -0,0 +1,29 @@
+package app.controllers;
+
+import app.exceptions.DatabaseException;
+import app.persistence.ConnectionPool;
+import app.services.CarportSvgTopDownView;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import java.util.Locale;
+
+public class SvgController {
+
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
+        app.get("/Svg", ctx -> displaySvg(ctx, connectionPool));
+    }
+
+    /**
+     * Renders the SVG page with top down view of carport with chosen width and length
+     *
+     * @param ctx            Context parameter used to obtain information related to the page
+     * @param connectionPool ConnectionPool used to retrieve data from database
+     */
+    public static void displaySvg(Context ctx, ConnectionPool connectionPool) {
+        Locale.setDefault(new Locale("US"));
+        CarportSvgTopDownView svg = new CarportSvgTopDownView(780, 600);
+        ctx.attribute("svg", svg.toString());
+        ctx.render("Svg-page.html");
+    }
+}
