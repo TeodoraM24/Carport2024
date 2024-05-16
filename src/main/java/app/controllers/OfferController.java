d6094203cb952394f16494a36fd52ce9a755724e
TeodoraM24 <teodora.milijic2402@gmail.com>
2024-05-16 11:55:08 +0200

    offer from customers side

--- src/main/java/app/controllers/OfferController.java
+++ src/main/java/app/controllers/OfferController.java
@@ -2,12 +2,16 @@ package app.controllers;
 
 import app.entities.Customer;
 import app.entities.Offer;
+import app.entities.PartsListItem;
+import app.entities.Price;
 import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
 import app.persistence.OfferMapper;
+import app.services.PriceCalculator;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import java.util.List;
 
 
 public class OfferController {
@@ -15,6 +19,7 @@ public class OfferController {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
             app.get("/offer/{offerId}", ctx -> getOffer(ctx, connectionPool));
             app.post("/offer/{offerId}/accept", ctx -> acceptOffer(ctx, connectionPool));
+            app.post("/offer/{offerId}/deny", ctx -> denyOffer(ctx, connectionPool));
         }
 
 
@@ -44,4 +49,17 @@ public class OfferController {
             ctx.status(500).result("Error updating offer status: " + e.getMessage());
         }
     }
+
+    public static void denyOffer(Context ctx, ConnectionPool connectionPool) {
+        try {
+            int offerId = Integer.parseInt(ctx.pathParam("offerId"));
+            OfferMapper.updateOfferStatus(offerId, "Afvist", connectionPool);
+            ctx.redirect("/deny-offer-page.html");
+        } catch (NumberFormatException e) {
+            ctx.status(400).result("Invalid offer ID");
+        } catch (DatabaseException e) {
+            ctx.status(500).result("Error updating offer status: " + e.getMessage());
+        }
+    }
+
 }
